5224d94371055b6b536121fd937339c3abbab704
Eric Norris <erictnorris@gmail.com>
2024-02-17 11:53:55 -0500

    feat: add `gdb` with custom auto-load safe-path

--- shell.nix
+++ shell.nix
@@ -2,11 +2,27 @@
 let
   pkgs  = import <nixpkgs> {};
   pkgs' = pkgs.pkgsCross.riscv64-embedded;
+
+  # we want to execute gdb automatically with the current directory added to the
+  # allow list
+  gdbWrapped = pkgs.symlinkJoin {
+    name = "gdb";
+
+    paths       = [ pkgs.gdb ];
+    buildInputs = [ pkgs.makeWrapper ];
+
+    postBuild = ''
+      wrapProgram $out/bin/gdb \
+        --add-flags "-iex \"set auto-load safe-path ${builtins.toString ./.}\""
+    '';
+  };
+
 in
   pkgs'.mkShell {
     depsBuildBuild = with pkgs; [
       gcc
       qemu
+      gdbWrapped
     ];
 
     nativeBuildInputs = with pkgs'.buildPackages; [
